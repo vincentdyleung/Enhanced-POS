3511b8ce49d155323e8deaa49bf66c9f961ff3c4
Vincent Leung <vincentleung1991@gmail.com>
2012-05-13 01:39:46 +0800

    default focus on usernameInput of LoginDialog

--- Enhanced_POS/src/gui/LoginDialog.java
+++ Enhanced_POS/src/gui/LoginDialog.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -109,6 +111,15 @@ public class LoginDialog extends JDialog {
 		} else {
 			warningButton.setText("Connection failed");
 		}
+		
+		addWindowListener(new WindowAdapter() {
+			/**
+			 * Focus on usernameInput when Login Dialog opens
+			 */
+			public void windowOpened(WindowEvent e) {
+				usernameInput.requestFocus();
+			}
+		});
 		this.pack();
 	}
 
